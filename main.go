dcd917f5480df2134c6c4107c215acea605141ca
Abenezer Mamo <major.abenezer@gmail.com>
2020-11-20 23:51:37 -0800

    refactored game play. added game play tests. added game play benchmarks. updated comments

--- main.go
+++ main.go
@@ -1,10 +1,68 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/abmamo/cards/blackjack"
 )
 
 func main() {
-	// start blackjack game
-	blackjack.Init()
+	// create var to get player name
+	var playerName string
+	// user prompt
+	fmt.Println("name:- ")
+	fmt.Println("-----------------")
+	// take input from user
+	fmt.Scanln(&playerName)
+	// init blackjack
+	b := blackjack.Init(playerName)
+	// run until user exits
+	for {
+		// show game info
+		b.Display()
+		// if no bet placed
+		if b.Player.CurrentBet == 0 {
+			// create var to get player buy
+			var betAmountStr string
+			// user prompt
+			fmt.Println("bet:- ")
+			fmt.Println("-----------------")
+			// take input from user & store
+			fmt.Scanln(&betAmountStr)
+			// convert to int
+			betAmount, _ := strconv.Atoi(betAmountStr)
+			// place bet
+			b.Player.Bet(betAmount)
+		}
+		// create var to get player action
+		var playerAction string
+		// user prompt
+		fmt.Println("action (hit/stand/double):- ")
+		fmt.Println("-----------------")
+		// take input from user & store
+		fmt.Scanln(&playerAction)
+		// start game play
+		b.Play(playerAction)
+		// check game state
+		if b.State == "over" {
+			// show game info
+			b.Display()
+			// user prompt
+			fmt.Println("another hand? (y/n)")
+			fmt.Println("-----------------")
+			// take input from user & store
+			fmt.Scanln(&playerAction)
+			// if another hand
+			if playerAction == "y" {
+				// start another game
+				b.Another()
+				// show info
+				b.Display()
+			} else {
+				// break
+				break
+			}
+		}
+	}
 }
